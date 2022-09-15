bbff3c9adb673772c1b34fbbfa00dcd06f88f996
Nicholas <blueretrograde@gmail.com>
2022-09-16 00:48:18 +0200

    done the first challenge

---
+++ script.js
@@ -0,0 +1,15 @@
+//1 
+
+function isNumberPositive(number) {
+
+        if (number >= 0)
+            console.log("Number " + number + " is positive")
+        else
+            console.log("Number " + number + " is negative")
+
+}
+
+isNumberPositive(3);
+
+//2
+
