bb5d67afb4a33498550938e6fc15768a7a315916
Nicholas <blueretrograde@gmail.com>
2022-09-16 01:03:53 +0200

    finished challenge 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//1 
+//1 - Negative or Positive
 
 function isNumberPositive(number) {
 
@@ -11,13 +11,22 @@ function isNumberPositive(number) {
 
 isNumberPositive(3);
 
-//2
+//2 - Days to Years
 
 function convertDaysToAge(days) {
     const daysInAYear = 365
     days = (days / daysInAYear)
-    console.log("The amount of years is " + days)
+    console.log("The amount of years is " + days);
 }
 
 convertDaysToAge(3680);
+
+//3 - Largest Number
+
+function getLargestNumber(number1, number2, number3) { 
+    console.log(Math.max(number1, number2, number3));
+
+}
+
+getLargestNumber(3, 18, 17);
 
